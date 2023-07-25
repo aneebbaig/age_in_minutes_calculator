29a2cc9ebd0785cba093c1e256ef3d888f3c68db
Aneeb Baig <aneebbaig123@gmail.com>
2023-07-25 22:47:55 +0500

    Final App

--- app/src/main/java/aneeb/practice/dobcalculator/MainActivity.kt
+++ app/src/main/java/aneeb/practice/dobcalculator/MainActivity.kt
@@ -13,13 +13,16 @@ import java.util.Locale
 class MainActivity : AppCompatActivity() {
 
     private var tvSelectedDate : TextView? = null
+    private var tvAgeInMinutes : TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val btnDatePicker : Button = findViewById(R.id.btnDatePicker)
+
         tvSelectedDate = findViewById(R.id.tvSelectedDate)
+        tvAgeInMinutes = findViewById(R.id.tvAgeInMinutes)
 
         btnDatePicker.setOnClickListener {
 
@@ -27,15 +30,14 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun clickDatePicker(){
+    private fun clickDatePicker(){
 
         val myCalendar = Calendar.getInstance()
         val year = myCalendar.get(Calendar.YEAR)
         val month = myCalendar.get(Calendar.MONTH)
         val day = myCalendar.get(Calendar.DAY_OF_MONTH)
-
-        DatePickerDialog(this,
-        DatePickerDialog.OnDateSetListener{view, selectedYear,selectedMonth,selectedDayOfMonth ->
+        val dpd = DatePickerDialog(this,
+        DatePickerDialog.OnDateSetListener{_, selectedYear,selectedMonth,selectedDayOfMonth ->
             Toast.makeText(this,"Date Picker Pressed",Toast.LENGTH_LONG).show()
 
 
@@ -47,16 +49,26 @@ class MainActivity : AppCompatActivity() {
 
             val theDate = sdf.parse(selectedDate)
 
+            theDate?.let {
+                val selectedDateInMinutes = theDate.time / 60000
+                val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
 
+                currentDate?.let {
+                    val currentDateInMinutes = currentDate.time / 60000
+                    val differenceInMinutes = currentDateInMinutes - selectedDateInMinutes
+                    tvAgeInMinutes?.text = differenceInMinutes.toString()
+}
 
+            }
 
         },
             year,
             month,
             day
 
-            ).show()
-
-
+            )
+        dpd.datePicker.maxDate = System.currentTimeMillis() - 86400000
+        dpd.show()
     }
+
 }
